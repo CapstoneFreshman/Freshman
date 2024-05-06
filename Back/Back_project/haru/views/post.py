3c199d695ff4b3003174f2497440aaf94b57d4e0
yireal <andytwo9878@gmail.com>
2024-05-06 19:28:28 +0900

    haru to s3

--- Back/Back_project/haru/views/post.py
+++ Back/Back_project/haru/views/post.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
+
+from .upload_file_to_s3 import upload_wav_to_s3
 from ..models import DIARY,DIARY_DETAIl
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseBadRequest
 from datetime import datetime
 from . import temp_voice
 from . import api_connector
@@ -15,18 +17,25 @@ def record(request):
     else:
         messages.warning(request, "로그인이 필요한 서비스입니다.")
         return render('webpage:index')
-    new_diary = DIARY.objects.all()
+    diary = DIARY.objects.all()
     if request.method == "POST":
         date = datetime.now()
         emo = request.POST['emo']
-        
-        if form.is_valid():
-            form.save()
-
-
-            return redirect('webpage:index')
+        response = upload_wav_to_s3(request)
+        if response.status_code == 200:
+            ori_file_path = response.json()['url']
+            new_diary = DIARY.objects.create(
+                user_id=user_id,
+                date=date,
+                emo=emo,
+                ori_file_path=ori_file_path
+            )
+            return HttpResponse("WAV file uploaded successfully.")
+        else:
+            # 업로드 실패 시, 에러 처리
+            return HttpResponseBadRequest("Failed to upload WAV file.")
     else:
-        form = CustomUserCreationForm()
+        return
 
 
 def call_haru(request):
---
+++ Back/Back_project/haru/views/upload_file_to_s3.py
@@ -0,0 +1,29 @@
+
+import boto3
+from django.conf import settings
+from django.http import JsonResponse
+
+def upload_wav_to_s3(request):
+    if request.method == 'POST' and request.FILES['wav_file']:
+        wav_file = request.FILES['wav_file']
+
+        # S3에 연결
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            region_name=settings.AWS_S3_REGION_NAME
+        )
+
+        # S3 버킷 이름
+        bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+
+        # S3에 WAV 파일 업로드
+        s3_client.upload_fileobj(wav_file, bucket_name, wav_file.name)
+
+        # 업로드된 파일의 URL 생성
+        s3_file_url = f"https://{bucket_name}.s3.amazonaws.com/{wav_file.name}"
+
+        return JsonResponse({'url': s3_file_url})
+    else:
+        return JsonResponse({'error': 'No WAV file uploaded'}, status=400)
