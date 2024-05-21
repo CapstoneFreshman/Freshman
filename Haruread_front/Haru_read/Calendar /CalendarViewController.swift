bca3a9f1ab258f62bdb546a7e82849c9d15173d2
jeon-syeon <108799852+jeon-syeon@users.noreply.github.com>
2024-05-22 01:07:12 +0900

    캘린더 기능 - 날짜 로드

--- Haruread_front/Haru_read/Calendar /CalendarViewController.swift
+++ Haruread_front/Haru_read/Calendar /CalendarViewController.swift
@@ -1,26 +1,48 @@
+
 import UIKit
+import FSCalendar
 
-class CalendarViewController: UIViewController{
-    let calendarView = UICalendarView()
-
+class CalendarViewController: UIViewController, FSCalendarDelegate {
+    
+    var calendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Initialize and configure the calendar
+        calendar = FSCalendar(frame: CGRect(x: 0, y: 200, width: self.view.bounds.width, height: 300))
+        view.addSubview(calendar)
+        
+        // Set the calendar delegate
+        calendar.delegate = self
+        
+        // Additional calendar configuration (optional)
+        calendar.scrollDirection = .horizontal
+        calendar.scope = .month
     }
     
-    func setupLabel() {
-        let label = UILabel()
-        let screenWidth = UIScreen.main.bounds.width
-        let labelWidth: CGFloat = 200
-        let labelHeight: CGFloat = 100
-        label.frame = CGRect(x: (screenWidth - labelWidth) / 2, y: 80, width: labelWidth, height: labelHeight)
-        label.text = "하루읽기"
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        label.font = UIFont(name: "HakgyoansimWoojuR", size: 50)
-        label.textColor = UIColor(red: 119/255, green: 78/255, blue: 61/255, alpha: 1.0)
-        self.view.addSubview(label)
+    // 캘린더 데이터 로드 함수!!!!!!
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let dateFormatter = Calendar.current
+                
+        // 날짜에서 년, 월, 일 추출
+        let year = dateFormatter.component(.year, from: date)
+        let month = dateFormatter.component(.month, from: date)
+        let day = dateFormatter.component(.day, from: date)
+                
+        // 년, 월, 일을 정수형으로 출력
+        print("년: \(year), 월: \(month), 일: \(day)")
     }
-
-
+    @IBAction func DiaryBtn(_ sender: Any) {
+        // Storyboard와 ViewController의 Identifier 확인 필요
+        let mystoryboard = UIStoryboard(name: "Main", bundle: nil)
+ 
+        let DiaryCheckViewController = mystoryboard.instantiateViewController(withIdentifier: "DiaryCheckViewController")
+        // 모달 전환 스타일 설정
+        DiaryCheckViewController.modalTransitionStyle = .crossDissolve
+        DiaryCheckViewController.modalPresentationStyle = .overFullScreen
+        
+        // 모달 방식으로 뷰 컨트롤러를 표시
+        self.present(DiaryCheckViewController, animated: true, completion: nil)
+    }
+    
 }
