df0e0c5209d75e1b87ac28479af8bf807ffbc3a9
yireal <andytwo9878@gmail.com>
2024-05-27 01:54:32 +0900

    인공지능 수정중

--- Back/Flask_ai/app.py
+++ Back/Flask_ai/app.py
@@ -10,26 +10,28 @@ app = Flask(__name__)
 def upload_file():
     if 'file' not in request.files:
         return BadRequest(description ="There is no file exists")
-    input_file = request.files['ori_file']
-    json_data = request.form.dict()
+    input_file = request.files['file']
+    json_data = request.get_json()
     if input_file.filename == '':
         return BadRequest(description ="No file name")
     ori_text = stt_file_whisper(input_file) #STT 텍스트 파일 가져오기
     result = total(
-        gendr = json_data['haru_gender'],
-        age_group = json_data['haru_age'],
-        speech_style = json_data['haru_style'],
-        emotion = json_data['emo'],
+        haru_gender=int(request.form['haru_gender']),
+        haru_age = int(request.form['haru_age']),
+        haru_style = int(request.form['haru_style']),
+        emo = request.form['emo'],
         diary = ori_text
     )#total에 필요한 정보 넣어서 result JSON + 파일 뭉치 가져오기
+    django_server_url = "http://127.0.0.1:8000/post/build/"
     output_file = result.file['feedback_file']
-    output_data = result.form.to_dict()
+    output_data = result.get_json()
     feedback_text = output_data.get('feedback_text')
     short_text = output_data.get('short_text')
     response_data = {'feedback_text': feedback_text,
                      'short_text' : short_text,
                }
-    response = requests.post(django_server_url,files = output_file,data = response_data)
+    files = {'feedback_file': (output_file.filename, output_file.stream, 'application/octet-stream')}
+    response = requests.post(django_server_url,files = files,data = response_data)
     #리스폰스를 만들쟈
     if response.status_code != 200:
         return BadRequest(description = "Fail to send to Django")
